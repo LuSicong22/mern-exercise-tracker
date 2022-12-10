11691043ac8cefc724fb11da85fbfcb21951485c
simon <simon.lu@tron.network>
2022-12-10 15:36:36 +0800

    feat: fix

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,7 +3,7 @@ let Exercise = require("../models/exercise.model");
 
 router.route("/").get((req, res) => {
   Exercise.find()
-    .then((exercises) => res.json(exercisess))
+    .then((exercises) => res.json(exercises))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri);
-
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
@@ -21,8 +20,8 @@ connection.once("open", () => {
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
 
-app.use("./exercises", exercisesRouter);
-app.use("./users", usersRouter);
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
